5b7f0c72194587357a07c5c0826c231d393eee18
alanna-wing <alanna.wing@gmail.com>
2017-01-08 19:26:40 +0000

    adding new files

---
+++ countries_hash.rb
@@ -0,0 +1,12 @@
+countries = {
+  uk: {
+    capital: "London",
+    population: 67000000
+  },
+  germany: {
+    capital: "Berlin",
+    population: 83000000
+  }
+}
+
+puts countries[:germany][:population]
